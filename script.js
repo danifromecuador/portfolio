ce9cc1fe002b2e0d6436b09741af9518326e9abe
dani morillo <danifromecuador@gmail.com>
2023-03-07 16:43:28 -0500

    Create the main div to show when the see-project button is clicked

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+// hamburger icon /////////////////////////////////////////
 const hamburger = document.querySelector('.hamburger-icon');
 const mobileMenu = document.querySelector('.mobile-menu');
 const body = document.querySelector('.body');
 const close = document.querySelector('.close');
 const toolbarMobileMenu = document.querySelector('.toolbar-mobile-menu');
 
+// see project ////////////////////////////////////////////
+const seeProject = document.querySelectorAll('.see-project');
+
+
+// hamburger icon /////////////////////////////////////////
 hamburger.addEventListener('click', () => {
   mobileMenu.classList.toggle('hidden'); // shows the mobile menu
-  // hamburger.classList.toggle('hidden'); // hides the hamburger icon
   body.classList.toggle('overflow');
 });
 
@@ -18,4 +23,26 @@ close.addEventListener('click', () => {
 toolbarMobileMenu.addEventListener('click', () => {
   mobileMenu.classList.toggle('hidden');
   body.classList.toggle('overflow');
+});
+
+// see project ////////////////////////////////////////////
+seeProject[0].addEventListener('click', () => {
+  const div = document.createElement('div');
+  
+  // Set the width and height styles of the div
+  div.style.width = '280px';
+  div.style.height = '480px';
+  div.style.backgroundColor = 'green';
+  div.style.position = 'fixed';
+  div.style.top = '50%';
+  div.style.left = '50%';
+  div.style.transform = 'translate(-50%, -50%)';
+  
+  
+  // Add some content to the div
+  div.textContent = 'This is a new div!';
+  
+  // Add the div to the page
+  document.body.appendChild(div);
+  
 });
