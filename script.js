127d969be6d7baa8181b901c110a3f27f332e4f2
Ruthmy <Ruryemo.exe@gmail.com>
2023-03-06 19:06:02 -0600

    Added teh first part of the auxiliary classes for the menu

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('hello world');
+
+let header = document.querySelector(".app-bar");
+let hamburger = document.querySelector("#hamburger");
+let menu = document.querySelector(".desktop-menu");
+
+hamburger.addEventListener("click", function(event) {
+  event.stopPropagation();
+  menu.classList.toggle("hidden");
+  menu.classList.toggle("desktop-menu__link");
+  header.classList.toggle("allViewHeight");
+});
