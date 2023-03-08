3b0d146a24d5fb7487237d1837e466d2295edfce
dani morillo <danifromecuador@gmail.com>
2023-03-08 16:25:06 -0500

    Create the popup window only in html and css

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const body = document.querySelector('.body');
 const close = document.querySelector('.close');
 const toolbarMobileMenu = document.querySelector('.toolbar-mobile-menu');
-
-// see project ////////////////////////////////////////////
-const seeProject = document.querySelectorAll('.see-project');
 // const projects = [
 //   {
 //     name: 'Project 1',
@@ -22,8 +19,6 @@ const seeProject = document.querySelectorAll('.see-project');
 //       github: 'https://github.com/danifromecuador',
 //   },
 // ];
-
-// hamburger icon /////////////////////////////////////////
 hamburger.addEventListener('click', () => {
   mobileMenu.classList.toggle('hidden'); // shows the mobile menu
   body.classList.toggle('overflow');
@@ -40,16 +35,7 @@ toolbarMobileMenu.addEventListener('click', () => {
 });
 
 // see project ////////////////////////////////////////////
-seeProject[0].addEventListener('click', () => {
-  // create the new main div
-  const div = document.createElement('div');
-  div.style.width = '280px';
-  div.style.height = '480px';
-  div.style.backgroundColor = 'green';
-  div.style.position = 'fixed';
-  div.style.top = '50%';
-  div.style.left = '50%';
-  div.style.transform = 'translate(-50%, -50%)';
-  div.textContent = 'This another is a new div! Just checking';
-  document.body.appendChild(div);
-});
+// const seeProject = document.querySelectorAll('.see-project');
+// seeProject[0].addEventListener('click', () => {
+//   alert('See Project');
+// });
