d71c60325ef892bd9dd04bdfc7a0ed6774d4af29
dani morillo <danifromecuador@gmail.com>
2023-03-08 10:55:52 -0500

    Fix linters errors

--- script.js
+++ script.js
@@ -7,17 +7,21 @@ const toolbarMobileMenu = document.querySelector('.toolbar-mobile-menu');
 
 // see project ////////////////////////////////////////////
 const seeProject = document.querySelectorAll('.see-project');
-const projects = [
-  {
-    name: 'Project 1',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aliquid cupiditate vitae tempore temporibus quo, eum pariatur. Neque sed ab in et architecto? In cumque obcaecati temporibus incidunt. A assumenda explicabo perspiciatis soluta tempora facilis suscipit culpa quo, alias nesciunt omnis similique quisquam fuga consectetur error laboriosam! Quis, quos repellendus.',
-    image: ['images/backgrounds/snapshoot-mobile.svg', 'images/backgrounds/snapshoot-desktop.svg'],
-    technologies: ['Ruby on rails', 'css', 'JavaScript', 'html'],
-    live: 'https://danifromecuador.github.io/portfolio/',
-    github: 'https://github.com/danifromecuador',
-  }
-];
+// const projects = [
+//   {
+//     name: 'Project 1',
+//     description:
+//       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis aliquid cupiditate
+//  vitae tempore temporibus quo, eum pariatur. Neque sed ab in et architecto? In cumque obcaec
+//  ati temporibus incidunt. A assumenda explicabo perspiciatis soluta tempora facilis suscipit
+//   culpa quo, alias nesciunt omnis similique quisquam fuga consectetur error laboriosam! Qui
+//   s, quos repellendus.',
+// image: ['images/backgrounds/snapshoot-mobile.svg', 'images/backgrounds/snapshoot-desktop.svg'],
+//     technologies: ['Ruby on rails', 'css', 'JavaScript', 'html'],
+//     live: 'https://danifromecuador.github.io/portfolio/',
+//     github: 'https://github.com/danifromecuador',
+//   },
+// ];
 
 // hamburger icon /////////////////////////////////////////
 hamburger.addEventListener('click', () => {
