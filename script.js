6c6a2b7ec88ceb61be6abf50c49b3e7b45b689fc
dani morillo <danifromecuador@gmail.com>
2023-03-08 22:19:42 -0500

    Show the modal window and close it but using inner in JS

--- script.js
+++ script.js
@@ -36,15 +36,37 @@ toolbarMobileMenu.addEventListener('click', () => {
 
 // see project ////////////////////////////////////////////
 const seeProject = document.querySelectorAll('.see-project');
-const popupWindow = document.querySelector('.popup-window');
-const popClose = document.querySelector('.pop-close');
+const melanie = document.querySelector('.melanie');
+const html = `<div class="popup-window">
+<div class="pop-image-and-close">
+  <img src="images/icons/pop-close.svg" alt="exit" class="pop-close hidden"></img>
+</div>
+<div class="pop-project-name">Keeping track of hundreds of components</div>
+<div class="pop-languages">
+  <h4 class="pop-languages-1">Ruby on rails</h4>
+  <h4 class="pop-languages-2">css</h4>
+  <h4 class="pop-languages-3">JavScript</h4>
+</div>
+<p class="pop-lorem">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea</p>
+<div class="pop-see-live-and-source">
+  <a href="https://danifromecuador.github.io/portfolio/" class="pop-see-live">
+    <h3 class="pop-see-live-text">See live</h3>
+    <img src="images/icons/see-live.svg" alt="" class="pop-see-live-image">
+  </a>
+  <a href="https://github.com/danifromecuador/portfolio" class="pop-see-source">
+    <h3 class="pop-see-source-text">See Source</h3>
+    <img src="images/icons/see-source.svg" alt="" class="pop-see-source-image">
+  </a>
+</div>
+</div>`;
 
 for (let i = 0; i < 6; i += 1) {
   seeProject[i].addEventListener('click', () => {
-    popupWindow.classList.toggle('hidden');
+    melanie.innerHTML = html;
+    const popClose = document.querySelector('.pop-close');
+    const popupWindow = document.querySelector('.popup-window');
+    popClose.addEventListener('click', () => {
+      popupWindow.classList.toggle('hidden');
+    });
   });
 }
-
-popClose.addEventListener('click', () => {
-  popupWindow.classList.toggle('hidden');
-});
