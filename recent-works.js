240219142fb451cd98803b92762109f9cd7a8a77
dani <danifromecuador@gmail.com>
2023-12-10 21:55:05 -0500

    Create an array of recent works

---
+++ recent-works.js
@@ -0,0 +1,44 @@
+let recentWorks = [
+  {
+    id: 1,
+    title: "Project 1",
+    category: "Web Development",
+    image: "https://picsum.photos/seed/picsum/200/300",
+    link: "https://www.google.com",
+  },
+  {
+    id: 2,
+    title: "Project 2",
+    category: "Web Development",
+    image: "https://picsum.photos/seed/picsum/200/300",
+    link: "https://www.google.com",
+  },
+  {
+    id: 3,
+    title: "Project 3",
+    category: "Web Development",
+    image: "https://picsum.photos/seed/picsum/200/300",
+    link: "https://www.google.com",
+  },
+  {
+    id: 4,
+    title: "Project 4",
+    category: "Web Development",
+    image: "https://picsum.photos/seed/picsum/200/300",
+    link: "https://www.google.com",
+  },
+  {
+    id: 5,
+    title: "Project 5",
+    category: "Web Development",
+    image: "https://picsum.photos/seed/picsum/200/300",
+    link: "https://www.google.com",
+  },
+  {
+    id: 6,
+    title: "Project 6",
+    category: "Web Development",
+    image: "https://picsum.photos/seed/picsum/200/300",
+    link: "https://www.google.com",
+  }
+];
